24062a78f94a629c72238985dedf0deed37865b4
sinamoraddar <sinamoraddar@gmail.com>
2020-01-19 18:27:40 +0330

    setup gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 module.exports = {
+  pathPrefix: "/website",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
