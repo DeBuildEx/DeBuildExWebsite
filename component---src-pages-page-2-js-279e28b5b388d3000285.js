b1abeeaf9d8ab6ced67671f267dcd9e88fec8ca6
sinamoraddar <sinamoraddar@gmail.com>
2020-01-19 18:26:08 +0330

    Updates

---
+++ component---src-pages-page-2-js-279e28b5b388d3000285.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),c=a("Wbzz"),l=a("CGcg"),r=a("4Tjr");t.default=function(){return o.a.createElement(l.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-279e28b5b388d3000285.js.map
---
+++ styles-f87ace6837a724695313.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{d1Pv:function(n,w,o){}}]);
+//# sourceMappingURL=styles-f87ace6837a724695313.js.map
