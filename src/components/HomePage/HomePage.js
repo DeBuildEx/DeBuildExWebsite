25fd641aa6a28e04fee53b514740a57bc29f64aa
sinamoraddar <sinamoraddar@gmail.com>
2020-02-01 12:18:33 +0330

    create the base of services section

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "src/components/HomePage/Header/Header"
 import Quotes from "src/components/HomePage/Quotes/Quotes"
+import Services from "src/components/HomePage/Services/Services"
 const HomePage = () => {
   return (
     <div>
       <Header />
       <Quotes />
+      <Services />
       this is the home page
     </div>
   )
